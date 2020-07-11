03210c4dd14bf0937781b626011f11c24111eeb7
JimdiGris <jimdigris@mail.ru>
2020-07-11 13:33:26 +0300

    в работе

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 'use strict';
 
 (function () {
-    const FILTER_BUTTON = document.querySelector('#filterButton');
 
-    FILTER_BUTTON.addEventListener('click', onFilterButtonClick);
+    const boxName = document.querySelectorAll('.box-name');         // получаем все параметры фильтрации    
+    const box = document.querySelectorAll('.box');                  // получаем все блоки, которые нужно отфильтровать
 
-    function onFilterButtonClick() {
-        console.log(getFilters());
-        console.log(getGoods());
-    }
+    boxName.forEach(function (item) {                               // перебираем все параметры фильтрации 
+        item.addEventListener('click', function (event) {           // на каждый параметр вешаем событие на клик
+            let filteringOption = event.target.dataset.filter;      // получаем значение параметра, который необходимо применить
+
+            box.forEach(function (item) {                                                           // перебираем все блоки, которые нужно отфильтровать
+                item.classList.remove('hide-block');                                                // очищаем прошлые добавления скрывающего классса
+
+                if (!item.classList.contains(filteringOption) && filteringOption !== 'all') {       // если нет класса соответстыующего праметру фильтра
+                    item.classList.add('hide-block');                                               // добавляем скрывающий класс
+                }
+            })
+
+        })
+    })
 })();
